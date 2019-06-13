ae14ba0ed2670b8ee1ef291cc495c28152852abc
tentenNT <takuminpc@gmail.com>
2019-06-13 02:24:05 +0000

    受け渡しをstr型からpathlib.PosixPath型に変更

--- na2McepClass.py
+++ na2McepClass.py
@@ -13,11 +13,11 @@ test_data_location = "city012"
 def readfiles(data_location):
     for i in range(1, 101):
         if i < 10:
-            yield Path(data_location+"/" + data_location + "_00{}.txt".format(i)).read_text()
+            yield Path(data_location+"/" + data_location + "_00{}.txt".format(i))
         elif i < 100:
-            yield Path(data_location+"/" + data_location + "_0{}.txt".format(i)).read_text()
+            yield Path(data_location+"/" + data_location + "_0{}.txt".format(i))
         else:
-            yield Path(data_location+"/" + data_location + "_{}.txt".format(i)).read_text()
+            yield Path(data_location+"/" + data_location + "_{}.txt".format(i))
  
 # 読み込み方
 # template_data_list = list(readfiles(template_data_location))
--- na2McepMain.py
+++ na2McepMain.py
@@ -9,3 +9,6 @@ a_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 b_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 11, 12, 13, 14, 15]
 print(mfunc.local_distance(a_array, b_array))
 
+template_data_list = list(mclass.readfiles(mclass.template_data_location))
+test_data_list = list(mclass.readfiles(mclass.test_data_location))
+print(type(template_data_list[0]))
