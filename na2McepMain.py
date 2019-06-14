ccacc5190ce2ad40b99c56dd599fceda37df53ec
tentenNT <takuminpc@gmail.com>
2019-06-14 20:48:52 +0000

    データの扱いをarrayにした．これでnpsavezが使えるはず

--- na2McepMain.py
+++ na2McepMain.py
@@ -6,7 +6,7 @@ import pdb
 def distance_checker(template_data, test_data): # フレーム同士での計算
     print(template_data.word, test_data.word)
 #     pdb.set_trace()
-    distance_array = np.zeros((len(template_data.framedata), len(test_data.framedata))) # タプルで渡す
+    distance_array_frames = np.zeros((len(template_data.framedata), len(test_data.framedata))) # タプルで渡す
 # 5フレーム用
 #     distance_array = np.zeros((5, 5)) # タプルで渡す
     for i in range(len(template_data.framedata)):
@@ -16,7 +16,7 @@ def distance_checker(template_data, test_data): # フレーム同士での計算
 # 上に同じ
 #         for j in range(5):
 #             print(i, j, np.linalg.norm(template_data.framedata[i] - test_data.framedata[j]))
-            distance_array[i][j] = np.linalg.norm(template_data.framedata[i] - test_data.framedata[j])
+            distance_array_frames[i][j] = np.linalg.norm(template_data.framedata[i] - test_data.framedata[j])
 #             print(distance_array)
 #             pdb.set_trace()
 
@@ -26,37 +26,31 @@ def distance_checker(template_data, test_data): # フレーム同士での計算
 # できねえ
 
 #     print(distance_array)
-    return distance_array
-
-# a_array = np.ndarray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
-# b_array = np.ndarray([1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 11, 12, 13, 14, 15])
-# print(mfunc.local_distance(a_array, b_array))
-# np.linalg.norm(a_array-b_array)
+    return distance_array_frames
 
 template_data_list = mclass.readfiles(mclass.template_data_location)
 test_data_list = mclass.readfiles(mclass.test_data_location)
 # print(template_data_list[2].framedata[0][4])
 # こうすればcity011_003.textの0フレーム目の3次元目が取れる
 
-# うっかり同じワード同士の比較しかやっていなかった
-# これでは意味がない…DPマッチングするためには全部計算する必要がある
-# for template_data, test_data in zip(template_data_list, test_data_list): というミス
-
 # 単語同士でのマッチング
 # distance_array = np.zeros((100, 100))
-# distance_array = np.zeros((5, 5))
-distance_list = []
+# distance_array_words = np.zeros((3, 3))
+distance_list = [[] for i in range(3)]
+# pdb.set_trace()
 distance_list_inside = []
 # フルサイズ(100*100)
-for template_data in template_data_list:
-    for test_data in test_data_list:
-        distance_checker(template_data, test_data)
+# for template_data in template_data_list:
+#     for test_data in test_data_list:
+#         distance_checker(template_data, test_data)
 
 # 3*3サイズ
-# for template_data in template_data_list[:3]:
-#     for test_data in test_data_list[:3]:
-#         distance_list_inside.append(distance_checker(template_data, test_data))
-#     distance_list.append(distance_list_inside)
+for i, template_data in enumerate(template_data_list[:3]):
+    for j, test_data in enumerate(test_data_list[:3]):
+        distance_list[i].append(distance_checker(template_data, test_data))
+
+
+# distance_array[:] = np.array(distance_list)
 
 # リスト内包表記にしてみる(100*100)
 # 次元を間違っている
@@ -66,5 +60,7 @@ for template_data in template_data_list:
 # distance_list = [distance_checker(template_data, test_data) for template_data in template_data_list[:5] for test_data in test_data_list[:5]]
 # print(distance_list[0])
 print("test")
-print(distance_list[0][0][60][54])
+print(distance_list)
+distance_array = np.array(distance_list)
+pdb.set_trace()
 # print(distance_list[99][5])
